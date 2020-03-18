6f709a73389443664396b1e2561d087b19a968cc
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-03-18 17:02:24 -0400

    Removed uikit, theme-ing using scss

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import "../styles/_globals.scss";
 class SignIn extends Component {
   render () {
     const { handleSignIn} = this.props; // going to come from App
     return(
       <div className="uk-background-primary">
         <div>
-          <h1 className="text-changer">Landing</h1>
+          <h1 className="test-sass">Landing</h1>
           <button onClick={handleSignIn}>Sign In With BlockStack</button>
         </div>
       </div>
