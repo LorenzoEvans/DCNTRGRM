2463cd5ef462f0eaa7a2055dc330735e47e750b5
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-03-19 12:11:32 -0400

    Saving changes

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,9 +4,10 @@ class SignIn extends Component {
   render () {
     const { handleSignIn} = this.props; // going to come from App
     return(
-      <div className="flex-row-between bg-primary">
+      <div className="flex-row-even">
         <h1 className="site-name">DCNTRGM</h1>
-          <button className="blockstack-sign-in" onClick={handleSignIn}>Sign In With BlockStack</button>
+        <h2>About</h2>
+        <button className="blockstack-sign-in" onClick={handleSignIn}>Sign In With BlockStack</button>
       </div>
     )
   }
